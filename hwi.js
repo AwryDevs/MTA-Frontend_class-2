ccd2d01c14e504c429c81e8b8b63bd185fac6a4a
AwryDevs <tundezbba2410@gmail.com>
2025-07-17 02:41:52 +0100

    Initial JavaScripts commit

---
+++ hwi.js
@@ -0,0 +1,57 @@
+function nameHwi(nara){
+    if(nara == "a"){
+        return "airplane"
+    }else if(nara == "b"){
+        return "ball"
+    }else if(nara == "c"){
+        return "cat"
+    }else if(nara == "d"){
+        return "dark"
+    }else if(nara == "e"){
+        return "egg"
+    }else if(nara == "f"){
+        return "flash"
+    }else if(nara == "g"){
+        return "gel"
+    }else if(nara == "h"){
+        return "horse"
+    }else if(nara == "i"){
+        return "immortal"
+    }else if(nara == "j"){
+        return "jug"
+    }else if(nara == "k"){
+        return "knife"
+    }else if(nara == "l"){
+        return "lamb"
+    }else if(nara == "m"){
+        return "man"
+    }else if(nara == "n"){
+        return "not"
+    }else if(nara == "o"){
+        return "orange"
+    }else if(nara == "p"){
+        return "pan"
+    }else if(nara == "q"){
+        return "queen"
+    }else if(nara == "r"){
+        return "run"
+    }else if(nara == "s"){
+        return "say"
+    }else if(nara == "t"){
+        return "tap"
+    }else if(nara == "u"){
+        return "umbrella"
+    }else if(nara == "v"){
+        return "van"
+    }else if(nara == "w"){
+        return "woman" 
+    }else if(nara == "x"){
+        return "x-ray"
+    }else if(nara == "y"){
+        return "yield"
+    }else if(nara == "z"){
+        return "zoo"
+    }else{
+        return "value not found"
+    }
+}
